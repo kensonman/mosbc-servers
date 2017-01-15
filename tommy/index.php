3e99826ecc71c1277944c12aec1a7c5cfb813982
Kenson Man <kenson@mosbc.org.hk>
2017-01-15 11:55:13 +0000

    Init commit: Day end

---
+++ tommy/index.php
@@ -0,0 +1,14 @@
+<?php
+	echo '
+	<!DOCTYPE html>
+	<html>
+		<head>
+			<title>Tommy Testing php file</title>
+			<meta charset="UTF-8">
+		</head>
+		<body>
+			<h1>Hello World! Using PHP...</h1>
+		</body>
+	</html>
+	';
+?>
